2a3b454cac79eb677f100971b4aa8f4587321ef5
shineunjeong <dkfah1229@naver.com>
2022-09-12 21:27:48 +0900

    컴포넌트 스캔으로 SpringBean 자동 설정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,10 +4,14 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+// Controller에서 외부 요청을 받고
 @Controller
 public class MemberController {
     private final MemberService memberService;
 
+
+    // Controller와 Service를 연결
+    // Controller 가 생성될 때 Spring Bean에 등록되어 있는 MemberService를 가져다가 넣어줌(DI : 의존 관계 주입)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+// Repository에서 데이터를 저장
 @Repository
 public class MemoryMemberRepository implements MemberRepository {
 
