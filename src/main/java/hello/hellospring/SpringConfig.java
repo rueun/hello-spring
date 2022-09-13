d6fd1edf8e68783774a961b49a57ccde56f19961
shineunjeong <dkfah1229@naver.com>
2022-09-14 01:52:33 +0900

    JPA 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,13 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 // 자바 코드로 직접 스프링 빈 등록하기
 // 실무에서는 주로 정형화 된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용.
@@ -15,12 +15,20 @@ import javax.sql.DataSource;
 // Controller는 어쩔 수 없이 @Controller 애노테이션과 @Autowired해준다.
 @Configuration
 public class SpringConfig {
+/*
     private DataSource dataSource;
-
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+*/
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -30,6 +38,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository(); // 여기서 만약 DB와 연결된 리포지토리로 변경하려면 다른 코드를 건들이지 않고, return new DBMemberRepository로 바꿔주기만 하면 됨.
-        return new JdbcMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    // @Id : 기본키, @GeneratedValue(strategy = GenerationType.IDENTITY) : DB에서 자동으로 생성된다는 것을 명시
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//  @Column(name = "username")
     private String name;
 
     public Long getId() {
